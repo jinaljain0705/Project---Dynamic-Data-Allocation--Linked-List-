5e3058c8b917e0b716ed9bc90f49f82cab974aa6
Jinal Jain <jinaljain624@gmail.com>
2025-07-01 14:37:36 +0530

    commit

---
+++ DynamicDataAllocation.cpp
@@ -0,0 +1,37 @@
+/*
+    Project Definition: Dynamic Memory Allocation
+
+    Develop a simple crud Opertaion in linked list with the help of C++ that will help you to
+    understand system and memory tp provide dynamically.
+
+    Project Criteria:
+    Requirements:
+        - Class & Object
+        - Constructors
+        - Methods
+
+    Class Structure:
+    1. Base Class: Dynamic Memory Allocation
+        Attributes:
+            - New node
+            - Next node
+            - Head
+        Methods:
+            - insert_at_beginning(data)
+            - Search(key)
+            - Delete_node(key)
+            - reverse()
+    
+    Requirements:
+    1. Basic Linked List Implementation.
+    2. Insert at Beginning & Search Operation.
+    3. Deletion of a Node.
+    4. Reversing the Linked List.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+}
